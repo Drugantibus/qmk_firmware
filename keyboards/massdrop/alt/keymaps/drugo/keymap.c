0d6a2f8ee29039fedab5c8552a1acd12d9329616
Alessandro Fort <drugantibus@pm.me>
2021-01-13 19:45:21 +0100

    Added macro layer

--- keyboards/massdrop/alt/keymaps/drugo/keymap.c
+++ keyboards/massdrop/alt/keymaps/drugo/keymap.c
@@ -12,9 +12,12 @@ enum alt_keycodes {
     MD_BOOT,               //Restart into bootloader after hold timeout
     RGB_FRZ,               //Freezes current RGB effect
     HID_SND,               //Send test HID
+    MCR_SIG,               //Signature macro
+    MCR_WTF,               //What the fuck did you say to me you little bitch?
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // DEFAULT
     [0] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
@@ -22,28 +25,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
+    // FUNCTION
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
-        F(0)   , RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______,U_T_AUTO,U_T_AGCR, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_HOME, \
-        _______,RGB_RMOD, RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, HID_SND, _______, _______, _______, _______, _______,          _______, _______, \
-        _______, RGB_TOG, RGB_FRZ, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, DBG_KBD,DBG_MTRX, _______, _______,          KC_VOLU, _______, \
-        DM_REC1, DM_RSTP, DM_PLY1,                            KC_MPLY,                            _______, _______, KC_MPRV, KC_VOLD, KC_MNXT  \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______,U_T_AUTO,U_T_AGCR, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_HOME, \
+        _______,RGB_RMOD, RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, HID_SND, _______, _______, _______, _______, _______,          _______, KC_BRIU, \
+        _______, RGB_TOG, RGB_FRZ, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, DBG_KBD,DBG_MTRX, _______, _______,          KC_VOLU, KC_BRID, \
+        _______, _______, _______,                            KC_MPLY,                            MO(2)  , _______, KC_MPRV, KC_VOLD, KC_MNXT  \
     ),
-    /*
-    [X] = LAYOUT(
+    // MACRO
+    [2] = LAYOUT_65_ansi_blocker(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
+        _______, _______, MCR_SIG, _______, _______, MCR_WTF, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        DM_REC1, DM_RSTP, DM_PLY1,                            _______,                            _______, _______, _______, _______, _______  \
     ),
-    */
+    
 };
 
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
 #define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+
+uint8_t layer_previous_state = LED_FLAG_ALL;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
@@ -88,6 +94,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        case MCR_SIG:
+            if (record->event.pressed) {
+                SEND_STRING("Best,\nAlessandro Fort");
+            }
+            return false;
+        case MCR_WTF:
+            if (record->event.pressed) {
+                SEND_STRING("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
+            }
+            return false;
+        case MO(2):
+            if (record->event.pressed) {
+                layer_previous_state = rgb_matrix_get_flags();
+                rgb_matrix_set_flags(LED_FLAG_NONE);
+                rgb_matrix_set_color_all(0, 0, 0);
+                rgb_matrix_set_color(46, 0, 255, 0); // X
+                rgb_matrix_set_color(49, 255, 0, 0); // B
+                rgb_matrix_set_color(58, 255, 50, 0); // LCTRL
+                rgb_matrix_set_color(59, 255, 50, 0); // LGUI
+                rgb_matrix_set_color(60, 255, 50, 0); // LALT
+                for (int i = 67; i < 105; i++){
+                    rgb_matrix_set_color(i, 0, 200, 50); // Underglow
+                }
+            }else {
+                rgb_matrix_set_flags(layer_previous_state);
+                rgb_matrix_set_color_all(0, 0, 0);
+            }
+            return true;
         // case HID_SND:
         //     if (record->event.pressed) {
         //         uint8_t data[RAW_EPSIZE];
@@ -193,9 +227,10 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
             }
         break;
         // Notifications
+        // Note: caller is expected to reset RGB to previous state
+        // TODO: set flags on single LEDs so RGB animation isn't interrupted on underglow
         case 2:
-            // Note: caller is expected to reset RGB to previous state
-            // TODO: set flags on single LEDs so RGB animation isn't interrupted on underglow
+            
             switch (data[1]) {
                 // Solid bottom underglow
                 case 1: {
