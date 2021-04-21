8edb74caf7581e9da52d5fd31a77e6825e4f5889
Drugantibus <drugantibus@pm.me>
2021-04-21 18:35:37 +0200

    Fixed build error

--- keyboards/massdrop/alt/keymaps/drugo/keymap.c
+++ keyboards/massdrop/alt/keymaps/drugo/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "raw_hid.h"
-// #include "print.h"
 
 enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
@@ -20,13 +19,16 @@ enum alt_keycodes {
     MCR_TVW,               //Teamviewer macro
     MCR_UCIS,              //Print configured UCIS entries
     UNI_ON,                //Startd UCIS (unicode input)
-    TD_LOCK_ALT,           //Tap Dance
+};
+
+enum {
+    TD_DEL_LOCK,           //Tap Dance
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // DEFAULT
     [0] = LAYOUT_65_ansi_blocker(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TD(TD_LOCK_ALT),  \
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TD_DEL_LOCK,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \
  CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
@@ -108,11 +110,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        #ifdef UCIS_ENABLE
         case UNI_ON:
             if (record->event.pressed) {
                 qk_ucis_start();
             }
-            return false;        
+            return false;     
+        #endif   
         case MCR_BST:
             if (record->event.pressed) {
                 SEND_STRING("\n\nBest,\nAlessandro Fort");
@@ -231,6 +235,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+#ifdef TAP_DANCE_ENABLE
 void lock_alt(qk_tap_dance_state_t *state, void *user_data){
     if (state->count == 1) {
         tap_code(KC_DEL);
@@ -243,8 +248,9 @@ void lock_alt(qk_tap_dance_state_t *state, void *user_data){
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LOCK_ALT] = ACTION_TAP_DANCE_FN(lock_alt),
+    [TD_DEL_LOCK] = ACTION_TAP_DANCE_FN(lock_alt),
 };
+#endif
 
 void set_led_range(int start, int stop, uint8_t r, uint8_t g, uint8_t b){
     for(int i = start; i <= stop; i++){
