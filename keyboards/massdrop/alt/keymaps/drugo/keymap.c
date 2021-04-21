343d45d3d076b23c570b2fe875074d4da0bc5f97
Alessandro Fort <alessandro.fort@gig.com>
2021-04-21 14:45:22 +0200

    Tap Dance Prototype

--- keyboards/massdrop/alt/keymaps/drugo/keymap.c
+++ keyboards/massdrop/alt/keymaps/drugo/keymap.c
@@ -20,12 +20,13 @@ enum alt_keycodes {
     MCR_TVW,               //Teamviewer macro
     MCR_UCIS,              //Print configured UCIS entries
     UNI_ON,                //Startd UCIS (unicode input)
+    TD_LOCK_ALT,           //Tap Dance
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // DEFAULT
     [0] = LAYOUT_65_ansi_blocker(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TD(TD_LOCK_ALT),  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \
  CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
@@ -229,6 +230,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; //Process all other keycodes normally
     }
 }
+
+void lock_alt(qk_tap_dance_state_t *state, void *user_data){
+    if (state->count == 1) {
+        tap_code(KC_DEL);
+    }
+    else{
+        register_code(KC_LGUI);
+        tap_code(KC_L);
+        unregister_code(KC_LGUI);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LOCK_ALT] = ACTION_TAP_DANCE_FN(lock_alt),
+};
 
 void set_led_range(int start, int stop, uint8_t r, uint8_t g, uint8_t b){
     for(int i = start; i <= stop; i++){
